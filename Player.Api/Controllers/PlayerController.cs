5b5e6fbdf2bc2f834c875bf67ba88e7274289e42
Kilsom <47924593+Kilsombg@users.noreply.github.com>
2022-08-18 19:57:46 +0300

    work in progress

---
+++ Player.Api/Controllers/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrettoPall.Api.Services;
+using PrettoPall.Data.Models;
+
+namespace PrettoPall.Api.Controllers
+{
+    [ApiController]
+    [Route("api/player")]
+    public class PlayerController : ControllerBase
+    {
+
+        private readonly MongoDBService mongoDBService;
+
+        public PlayerController(MongoDBService mongoDBService)
+        {
+            this.mongoDBService = mongoDBService;
+        }
+
+        [HttpGet]
+        public async Task<List<Player>> Get() {
+            return await mongoDBService.GetAsync();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Player player) {
+            await mongoDBService.CreateAsync(player);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AddToPlaylist(string id, [FromBody] string movieId) {
+                await mongoDBService.AddToPlaylistAsync(id, movieId);
+                return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id) {
+            return Ok();
+        }
+
+    }
+}
+
---
+++ Player.Data/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace PrettoPall.Data.Models
+{
+	public class Player
+	{
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+
+    }
+}
